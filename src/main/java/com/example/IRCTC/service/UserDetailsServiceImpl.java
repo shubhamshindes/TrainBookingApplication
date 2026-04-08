fc3a37873f27de04771dec390c6ad6ef8a39ad6e
shubhamshindes <shubhamshindes.dev@gmail.com>
2026-04-09 00:55:53 +0530

    updated

--- src/main/java/com/example/IRCTC/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/IRCTC/service/UserDetailsServiceImpl.java
@@ -19,13 +19,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Optional<User> userOptional = userRepository.findByUserName(username);
+		Optional<User> userOptional = userRepository.findByUserName(username);// we took username from DB.
 
 		if (userOptional.isPresent()) {
 			User user = userOptional.get(); // Extract User from Optional
 			return org.springframework.security.core.userdetails.User.builder()
 					.username(user.getUserName()) //Sets the username.
-					.password(user.getPassword())//Sets the hashed password (stored in DB).
+					.password(user.getPassword())//Sets the hashed password (stored in DB).hashed password (DB से लिया गया)
 					.roles(user.getRoles().toArray(new String[0])) //Converts user roles (e.g., ROLE_ADMIN) into a String[].
 					.build();
 		}
